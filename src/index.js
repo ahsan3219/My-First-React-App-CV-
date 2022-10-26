1522d1867ab66c40d3a36645ad0c19ebefda612b
MuhammadNaeemRaza2916 <muhammadnaeemraza16@gmail.com>
2022-10-26 19:26:04 +0500

    Updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Zaid() {
+  return (
+  <center><form><fieldset>
+  <center> <legand>Resume</legand></center>
+   <div className="abcd">
+     
+     <h1>
+       <u>
+         <i className="name">"Ahsan Raza"</i>
+       </u>{" "}
+     </h1>
+     <h4>Phone No:</h4> 03112324421
+     <br />
+     <h4>E-Mail:</h4> Ahsanrazasyedahsan@gmail.com
+     <p>
+       <b>
+         {" "}
+         <u>My Goals:</u>
+       </b>
+     </p>
+     <p>
+       To secure a challenging position in software house to enhance my learnings,
+       knowledge, and skills.
+     </p>
+     <p>
+       <b>
+         {" "}
+         <u>MY EDUCATION:</u>
+       </b>
+     </p>
+     <ul>
+      
+           SCHOOL:
+         <a>Happy palace</a>
+  
+  <br/>
+         <i>
+           <u>COLLEGE:</u>
+         </i>
+         <a>Dehli College</a>
+   
+  <br/>
+         <i>
+           <u>UNIVERSITY:</u>
+         </i>
+         <a>NED UNIVERSITY</a>
+       
+       </ul>
+     <p>
+       <b>
+         <u>MY SKILLS:</u>
+       </b>
+     </p>
+     <ul>
+       Active Listening.
+  <br/>
+       programming.
+       
+  <br/>HTML CSS.
+       
+  <br/>Computer Skills.
+       
+  <br/>Problem Solving Skills.
+     </ul>
+     <p>
+       <b>
+         <u>MY ACTIVITIES:</u>
+       </b>
+     </p>
+ 
+     <ul>
+       Reading books.
+       
+  <br/>Painting.
+       
+  <br/>Learning Other Languages.
+       
+  <br/>Participation in competitions.
+     </ul>
+   </div> </fieldset></form></center>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Zaid />, document.querySelector("#root"));
